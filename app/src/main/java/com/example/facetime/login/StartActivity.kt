570e4de82d14a840eb4a586fa4bd021763e98d79
武起杰 <1982321927@qq.com>
2019-11-01 11:11:45 +0800

    登录重置密码页面编写

---
+++ app/src/main/java/com/example/facetime/login/StartActivity.kt
@@ -0,0 +1,134 @@
+package com.example.facetime.login
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Gravity
+import androidx.appcompat.app.AppCompatActivity
+import com.example.facetime.R
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
+class StartActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        super.onCreate(savedInstanceState)
+
+        linearLayout {
+            backgroundColor = Color.parseColor("#F2F2F2")
+           verticalLayout{
+               backgroundColor = Color.parseColor("#F2F2F2")
+                textView {
+                    text = "注册"
+                    gravity = Gravity.RIGHT
+                    textSize = 16f
+                    textColor = Color.parseColor("#7F7F7F")
+                }.lparams(height = wrapContent,width = matchParent){
+                    topPadding = dip(20)
+                }
+
+               textView {
+                   text = "登录"
+                   gravity = Gravity.CENTER
+                   textSize = 21f
+                   textColor = Color.parseColor("#333333")
+               }.lparams(height = wrapContent,width = matchParent){
+                    topMargin = dip(75)
+               }
+
+               linearLayout {
+                   backgroundResource = R.drawable.input_border
+                   textView {
+                       text = "+81"
+                       gravity = Gravity.CENTER
+                       backgroundColor = Color.WHITE
+                       textSize = 15f
+                       textColor = Color.parseColor("#333333")
+                   }.lparams(height = matchParent,width = wrapContent){
+                       leftMargin = dip(5)
+                       rightMargin = dip(10)
+                   }
+
+                   editText {
+                       hint = "请输入手机号码"
+                       backgroundColor = Color.WHITE
+                   }.lparams(width = matchParent,height = wrapContent){
+                       weight = 1f
+                   }
+               }.lparams(height = wrapContent,width = matchParent){
+                   topMargin = dip(25)
+               }
+
+               linearLayout {
+                   backgroundResource = R.drawable.input_border
+                   textView {
+
+                   }.lparams(width = wrapContent,height = matchParent){
+                        topMargin = dip(10)
+                   }
+
+                   editText {
+                       hint = "请输入密码"
+                       backgroundColor = Color.WHITE
+                   }.lparams(width = matchParent,height = wrapContent){
+                       weight = 1f
+                   }
+               }.lparams(height = wrapContent,width = matchParent){
+
+                   topMargin = dip(10)
+                   leftPadding = dip(10)
+                   rightPadding = dip(10)
+               }
+
+               linearLayout {
+                   checkBox {
+                   }
+
+                   textView {
+                       text = "我同意"
+                   }
+                   textView {
+                       text = "隐私协议"
+                       textColor = Color.parseColor("#44CDF6")
+                   }
+                   textView {
+                       text = "和"
+                   }
+                   textView {
+                       text = "服务声明"
+                       textColor = Color.parseColor("#44CDF6")
+                   }
+               }.lparams(){
+                   topMargin = dip(15)
+               }
+
+
+               button {
+                   backgroundResource = R.drawable.login_button
+                   text = "登录"
+                   textColor = Color.WHITE
+                   textSize = 21f
+               }.lparams(width = matchParent,height = wrapContent){
+                   topMargin = dip(30)
+               }
+
+               textView {
+                   text = "忘记密码"
+                   textColor = Color.parseColor("#7F7F7F")
+                   gravity = Gravity.RIGHT
+                   onClick {
+                       startActivity<ReadSetPasswordActivity>()
+                   }
+               }.lparams(height = wrapContent,width = matchParent){
+                   topMargin = dip(25)
+               }
+
+           }.lparams(width = matchParent,height = matchParent){
+                leftMargin = dip(20)
+                rightMargin = dip(20)
+           }
+        }
+    }
+
+}
