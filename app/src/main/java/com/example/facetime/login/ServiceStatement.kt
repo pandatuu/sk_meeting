ba654b5d4a9056af52b57774291fc8f1e5ea9d31
武起杰 <1982321927@qq.com>
2019-11-06 15:12:52 +0800

    协议页面部分修改

--- app/src/main/java/com/example/facetime/login/ServiceStatement.kt
+++ app/src/main/java/com/example/facetime/login/ServiceStatement.kt
@@ -1,34 +1,53 @@
 package com.example.facetime.login
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import com.example.facetime.R
 import org.jetbrains.anko.*
 
 class ServiceStatement: AppCompatActivity() {
+    lateinit var statement:WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         linearLayout {
             scrollView {
-                webView {
+                statement = webView {
                     //                    loadUrl("https://o.skjob.jp/login/serviceforios")
                     this.settings.javaScriptEnabled = true
+
                     //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
                     this.webViewClient = object : WebViewClient() {
                         override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
                             view?.loadUrl(request?.url.toString())
                             return true
                         }
+
+                        // 请求发送，等待过程
+                        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                            super.onPageStarted(view, url, favicon)
+                        }
+
+                        // 请求成功，返回结果，取消提示
+                        override fun onPageFinished(view: WebView?, url: String?) {
+                            super.onPageFinished(view, url)
+                        }
                     }
-                    this.loadUrl("https://www.baidu.com")
+                    this.loadUrl(R.string.serviceStatementUrl.toString())
                 }.lparams(width = matchParent,height = matchParent){}
             }.lparams(width = matchParent,height = wrapContent){
                 weight = 1f
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        statement.destroy()
     }
 }
