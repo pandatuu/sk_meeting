a6606f99f0e2cb45f60db51d03ffbc4579d15306
LIZHENCHUAN <395387944@qq.com>
2019-11-08 16:12:53 +0800

    添加更新功能

--- app/src/main/java/com/example/facetime/conference/MenuActivity.kt
+++ app/src/main/java/com/example/facetime/conference/MenuActivity.kt
@@ -1,5 +1,6 @@
 package com.example.facetime.conference
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
@@ -8,7 +9,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.jaeger.library.StatusBarUtil
 import org.jetbrains.anko.*
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.preference.PreferenceManager
+import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.widget.*
@@ -16,16 +20,20 @@ import com.example.facetime.R
 import kotlin.system.exitProcess
 import com.dropbox.core.v2.teamlog.ActorLogInfo.app
 import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.FragmentTransaction
 import com.example.facetime.login.StartActivity
 
 
-open class MenuActivity : AppCompatActivity() {
-
+open class MenuActivity : AppCompatActivity(),UpdateTipsFrag.ButtomCLick,
+    BackgroundFragment.ClickBack{
 
     private lateinit var toolbar1: Toolbar
+    private var shadowFragment: BackgroundFragment? = null
+    private var updateTips: UpdateTipsFrag? = null
 
-    private  lateinit var createMy:TextView
-    private  lateinit var enterMy:TextView
+    private var fram: FrameLayout? = null
+    private lateinit var createMy:TextView
+    private lateinit var enterMy:TextView
     private var exitTime: Long = 0
 
     override fun onActivityResult(
@@ -43,105 +51,144 @@ open class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         val mainId = 1
-        verticalLayout {
+        fram = frameLayout {
+            id = mainId
             backgroundColor = Color.parseColor("#f2f2f2")
-            //toolbar
-            relativeLayout() {
-                backgroundColor=Color.TRANSPARENT
-                toolbar1 = toolbar {
+            linearLayout {
+                orientation = LinearLayout.VERTICAL
+                //toolbar
+                relativeLayout() {
                     backgroundColor=Color.TRANSPARENT
-                    isEnabled = true
-                    title = ""
+                    toolbar1 = toolbar {
+                        backgroundColor=Color.TRANSPARENT
+                        isEnabled = true
+                        title = ""
+                    }.lparams() {
+                        width = matchParent
+                        height = dip(0)
+                        alignParentBottom()
+
+                    }
                 }.lparams() {
                     width = matchParent
                     height = dip(0)
-                    alignParentBottom()
-
                 }
-            }.lparams() {
-                width = matchParent
-                height = dip(0)
-            }
+                linearLayout() {
+                    gravity = Gravity.RIGHT or Gravity.CENTER_VERTICAL
 
+                    imageView() {
+                        imageResource = R.mipmap.settings
+                        setOnClickListener {
 
+                            var intent = Intent(this@MenuActivity, SystemSettingsActivity::class.java)
+                            startActivityForResult(intent, 2)
+                            overridePendingTransition(
+                                R.anim.right_in,
+                                R.anim.left_out
+                            )
 
-            linearLayout() {
-                gravity = Gravity.RIGHT or Gravity.CENTER_VERTICAL
+                        }
 
-                imageView() {
-                    imageResource = R.mipmap.settings
-                    setOnClickListener {
-
-                        var intent = Intent(this@MenuActivity, SystemSettingsActivity::class.java)
-                        startActivityForResult(intent, 2)
-                        overridePendingTransition(
-                            R.anim.right_in,
-                            R.anim.left_out
-                        )
-
+                    }.lparams() {
+                        height = dip(20)
+                        width = dip(20)
+                        topMargin = dip(5)
+                        rightMargin = dip(20)
                     }
 
                 }.lparams() {
-                    height = dip(20)
-                    width = dip(20)
-                    topMargin = dip(5)
-                    rightMargin = dip(20)
+                    width = matchParent
+                    height = dip(100)
                 }
 
-            }.lparams() {
-                width = matchParent
-                height = dip(100)
-            }
-
-            linearLayout() {
-                gravity = Gravity.CENTER
-                textView {
-                    text = "欢迎使用SK-Meeting在线视频会议系统"
-                    textSize = 20f
-                    textColor = Color.BLACK
+                linearLayout() {
                     gravity = Gravity.CENTER
-                    typeface = Typeface.DEFAULT_BOLD
+                    textView {
+                        text = "欢迎使用SK-Meeting在线视频会议系统"
+                        textSize = 20f
+                        textColor = Color.BLACK
+                        gravity = Gravity.CENTER
+                        typeface = Typeface.DEFAULT_BOLD
+                    }.lparams() {
+                        height = dip(40)
+                        width = wrapContent
+                    }
+
                 }.lparams() {
-                    height = dip(40)
-                    width = wrapContent
+                    width = matchParent
+                    height = wrapContent
+                    topMargin = dip(20)
                 }
 
-            }.lparams() {
-                width = matchParent
-                height = wrapContent
-                topMargin = dip(20)
-            }
 
+                verticalLayout {
+                    gravity = Gravity.CENTER_HORIZONTAL
 
-            verticalLayout {
-                gravity = Gravity.CENTER_HORIZONTAL
-
-              createMy=  textView {
-                    text = "创建我的会议室"
-                    textSize = 14f
-                    textColor = Color.WHITE
-                    backgroundResource = R.drawable.bottonbg
-                    gravity = Gravity.CENTER
-                    visibility=View.GONE
-                    setOnClickListener {
+                    createMy=  textView {
+                        text = "创建我的会议室"
+                        textSize = 14f
+                        textColor = Color.WHITE
+                        backgroundResource = R.drawable.bottonbg
+                        gravity = Gravity.CENTER
+                        visibility=View.GONE
+                        setOnClickListener {
 
 
 
 
-                        val sp = PreferenceManager.getDefaultSharedPreferences(context)
-                        val token = sp.getString("token", "")
-                        println("本机token为：$token")
+                            val sp = PreferenceManager.getDefaultSharedPreferences(context)
+                            val token = sp.getString("token", "")
+                            println("本机token为：$token")
 
-                        if (token.isNullOrEmpty()) {
-                            startActivity<StartActivity>()
-                            overridePendingTransition(
-                                R.anim.fade_in_out,
-                                R.anim.fade_in_out
-                            )
-                        } else {
-                            startActivity<CreateRoomNameActivity>()
+                            if (token.isNullOrEmpty()) {
+                                startActivity<StartActivity>()
+                                overridePendingTransition(
+                                    R.anim.fade_in_out,
+                                    R.anim.fade_in_out
+                                )
+                            } else {
+                                startActivity<CreateRoomNameActivity>()
+                                overridePendingTransition(
+                                    R.anim.right_in,
+                                    R.anim.left_out
+                                )
+                            }
+
+
+                        }
+
+
+                    }.lparams() {
+                        height = dip(40)
+                        width = dip(240)
+                        topMargin = dip(20)
+                    }
+
+
+                    enterMy=  textView {
+                        text = "进入我的会议室"
+                        textSize = 14f
+                        textColor = Color.WHITE
+                        backgroundResource = R.drawable.bottonbg
+                        gravity = Gravity.CENTER
+                        visibility=View.GONE
+
+
+                        setOnClickListener {
+
+                            var intentNow =
+                                Intent(this@MenuActivity, SuccessActivity::class.java)
+
+
+
+                            var MyRoomName =
+                                PreferenceManager.getDefaultSharedPreferences(this@MenuActivity)
+                                    .getString("MyRoomName", "").toString()
+
+
+                            intentNow.putExtra("RoomName",MyRoomName)
+                            startActivityForResult(intentNow, 50)
                             overridePendingTransition(
                                 R.anim.right_in,
                                 R.anim.left_out
@@ -149,94 +196,57 @@ open class MenuActivity : AppCompatActivity() {
                         }
 
 
-                    }
 
-
-                }.lparams() {
-                    height = dip(40)
-                    width = dip(240)
-                    topMargin = dip(20)
-                }
-
-
-              enterMy=  textView {
-                    text = "进入我的会议室"
-                    textSize = 14f
-                    textColor = Color.WHITE
-                    backgroundResource = R.drawable.bottonbg
-                    gravity = Gravity.CENTER
-                    visibility=View.GONE
-
-
-                    setOnClickListener {
-
-                        var intentNow =
-                            Intent(this@MenuActivity, SuccessActivity::class.java)
-
-
-
-                        var MyRoomName =
-                            PreferenceManager.getDefaultSharedPreferences(this@MenuActivity)
-                                .getString("MyRoomName", "").toString()
-
-
-                        intentNow.putExtra("RoomName",MyRoomName)
-                        startActivityForResult(intentNow, 50)
-                        overridePendingTransition(
-                            R.anim.right_in,
-                            R.anim.left_out
-                        )
+                    }.lparams() {
+                        height = dip(40)
+                        width = dip(240)
+                        topMargin = dip(20)
                     }
 
 
 
-                }.lparams() {
-                    height = dip(40)
-                    width = dip(240)
-                    topMargin = dip(20)
-                }
+                    textView {
+                        text = "加入一个会议室"
+                        textSize = 14f
+                        textColor = Color.WHITE
+                        backgroundResource = R.drawable.bottonbg
+                        gravity = Gravity.CENTER
 
 
 
-                textView {
-                    text = "加入一个会议室"
-                    textSize = 14f
-                    textColor = Color.WHITE
-                    backgroundResource = R.drawable.bottonbg
-                    gravity = Gravity.CENTER
+                        setOnClickListener {
+
+                            var intent = Intent(this@MenuActivity, EnteRoomByIdActivity::class.java)
+                            startActivityForResult(intent, 12)
+                            overridePendingTransition(
+                                R.anim.right_in,
+                                R.anim.left_out
+                            )
+
+                        }
 
 
-
-                    setOnClickListener {
-
-                        var intent = Intent(this@MenuActivity, EnteRoomByIdActivity::class.java)
-                        startActivityForResult(intent, 12)
-                        overridePendingTransition(
-                            R.anim.right_in,
-                            R.anim.left_out
-                        )
+                    }.lparams() {
+                        height = dip(40)
+                        width = dip(240)
+                        topMargin = dip(20)
 
                     }
 
-
                 }.lparams() {
-                    height = dip(40)
-                    width = dip(240)
-                    topMargin = dip(20)
-
+                    width = matchParent
+                    height = wrapContent
+                    topMargin = dip(40)
                 }
-
-            }.lparams() {
-                width = matchParent
-                height = wrapContent
-                topMargin = dip(40)
             }
-
-
+        }
+        //检查更新
+        val version = getLocalVersion(this@MenuActivity)
+        if (version < 2) {
+            opendialog()
         }
 
         isRoomCreated()
-
     }
 
 
@@ -291,6 +301,76 @@ open class MenuActivity : AppCompatActivity() {
             }
         }
         return super.onKeyDown(keyCode, event)
+    }
+
+    // 获取软件的版本号 versionCode,用于比较版本
+    private fun getLocalVersion(ctx: Context): Int {
+        var localVersion = 0
+        try {
+            val packageInfo = ctx.applicationContext
+                .packageManager
+                .getPackageInfo(ctx.packageName, 0)
+            localVersion = packageInfo.versionCode
+            Log.d("TAG", "本软件的版本号。。$localVersion")
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+
+        return localVersion
+    }
+
+    //打开更新弹窗
+    private fun opendialog() {
+        println("要更新")
+        //如果版本低,弹出更新弹窗
+        val mTransaction = supportFragmentManager.beginTransaction()
+        mTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        if (shadowFragment == null) {
+            shadowFragment = BackgroundFragment.newInstance()
+            mTransaction.add(fram!!.id, shadowFragment!!)
+        }
+        if (updateTips == null) {
+            updateTips = UpdateTipsFrag.newInstance(this@MenuActivity)
+            mTransaction.add(fram!!.id, updateTips!!)
+        }
+        mTransaction.commit()
+
+    }
+    //关闭弹窗
+    private fun closeAlertDialog() {
+        val mTransaction = supportFragmentManager.beginTransaction()
+        if (updateTips != null) {
+            mTransaction.setCustomAnimations(
+                R.anim.fade_in_out, R.anim.fade_in_out
+            )
+            mTransaction.remove(updateTips!!)
+            updateTips = null
+        }
+
+        if (shadowFragment != null) {
+            mTransaction.setCustomAnimations(
+                R.anim.fade_in_out, R.anim.fade_in_out
+            )
+            mTransaction.remove(shadowFragment!!)
+            shadowFragment = null
+        }
+        mTransaction.commit()
+    }
+    //不更新
+    override fun cancelUpdateClick() {
+        closeAlertDialog()
+        finish()
+    }
+    //要更新
+    override fun defineClick(downloadUrl: String) {
+        val uri = Uri.parse(downloadUrl)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+        closeAlertDialog()
+    }
+    //点击屏幕
+    override fun clickAll() {
+        closeAlertDialog()
     }
 
 }
---
+++ app/src/main/java/com/example/facetime/conference/UpdateTipsFrag.kt
@@ -0,0 +1,119 @@
+package com.example.facetime.conference
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.example.facetime.R
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.support.v4.UI
+
+class UpdateTipsFrag : Fragment() {
+
+    lateinit var mContext: Context
+    private lateinit var buttomCLick : ButtomCLick
+    private lateinit var imageV : ImageView
+
+    companion object {
+        fun newInstance(
+            context: Context
+        ):UpdateTipsFrag{
+            val fragment = UpdateTipsFrag()
+            fragment.mContext = context
+            return fragment
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        buttomCLick = activity as ButtomCLick
+        val view = createV()
+
+        Glide.with(this@UpdateTipsFrag)
+            .asBitmap()
+            .load("")
+            .into(imageV)
+        return view
+    }
+
+    private fun createV(): View? {
+        return with(mContext!!){
+            linearLayout {
+                gravity = Gravity.CENTER
+                linearLayout {
+                    isClickable = true
+                    orientation = LinearLayout.VERTICAL
+                    backgroundResource = R.drawable.fourdp_white_dialog
+                    linearLayout {
+                        orientation = LinearLayout.VERTICAL
+                        imageV = imageView {
+                            adjustViewBounds = true
+                            scaleType = ImageView.ScaleType.FIT_XY
+                        }.lparams(wrapContent, dip(110)){
+                            gravity = Gravity.CENTER_HORIZONTAL
+                        }
+                        scrollView {
+                            isVerticalScrollBarEnabled = false
+                            textView {
+                                text = "111111111111111"
+                                textSize = 13f
+                                setLineSpacing(2f,1f)
+                            }.lparams(wrapContent, wrapContent)
+                        }.lparams(matchParent, dip(0)){
+                            leftMargin = dip(15)
+                            rightMargin = dip(10)
+                            topMargin = dip(5)
+                            weight = 1f
+                        }
+                    }.lparams(matchParent,dip(0)){
+                        weight = 1f
+                    }
+                    relativeLayout {
+                        button {
+                            text = "キャンセル"
+                            textSize = 16f
+                            textColor = Color.WHITE
+                            backgroundResource = R.drawable.button_shape_grey
+                            onClick {
+                                buttomCLick.cancelUpdateClick()
+                            }
+                        }.lparams(dip(120),dip(40)){
+                            topMargin = dip(20)
+                            alignParentLeft()
+                        }
+                        button {
+                            text = "確定"
+                            textSize = 16f
+                            textColor = Color.WHITE
+                            backgroundResource = R.drawable.yellow_background
+                           onClick {
+                                buttomCLick.defineClick("https://www.baidu.com")
+                            }
+                        }.lparams(dip(120),dip(40)){
+                            topMargin = dip(20)
+                            alignParentRight()
+                        }
+                    }.lparams(matchParent,dip(93)){
+                        leftMargin = dip(15)
+                        rightMargin = dip(15)
+                    }
+                }.lparams(matchParent,dip(400)){
+                    leftMargin = dip(38)
+                    rightMargin = dip(38)
+                }
+            }
+        }
+    }
+
+    interface ButtomCLick{
+        fun cancelUpdateClick()
+        fun defineClick(downloadUrl: String)
+    }
+}
